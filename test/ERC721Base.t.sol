847182a1161327b0b359771d2beaa2f58919f2e0
Arthur Chen <arthur_x_chen@proton.me>
2025-06-30 10:23:30 -0700

    add tests

--- test/ERC721Base.t.sol
+++ test/ERC721Base.t.sol
@@ -5,20 +5,32 @@ import {Test, console} from "forge-std/Test.sol";
 import {ERC721Base} from "../src/ERC721Base.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
+contract ERC721Wallet is ERC721Holder {
+
+}
+
 contract ERC721BaseTest is Test, ERC721Holder {
     ERC721Base public nftToken;
+    ERC721Wallet public wallet;
 
     function setUp() public {
         nftToken = new ERC721Base("GameToken", "GT", "ipfs://");
+        wallet = new ERC721Wallet();
     }
 
     function test_mint () public {
-        nftToken.safeMint(address(this), "1234");
-        assertEq(nftToken.tokenURI(0), "ipfs://1234");
+        uint256 tokenId = nftToken.safeMint(address(this), "1234");
+        assertEq(tokenId, 0);
+        assertEq(nftToken.tokenURI(tokenId), "ipfs://1234");
+
+        tokenId = nftToken.safeMint(address(this), "5678");
+        assertEq(tokenId, 1);
+        assertEq(nftToken.tokenURI(tokenId), "ipfs://5678");
     }
 
-    /*function test_transfer(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }*/
+    function test_transfer() public {
+        uint256 tokenId = nftToken.safeMint(address(this), "9999");
+        nftToken.safeTransferFrom(address(this), address(wallet), tokenId);
+        assertEq(nftToken.ownerOf(tokenId), address(wallet));
+    }
 }
